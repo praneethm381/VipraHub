9b37b22226249f666cc7d958825d3271b2e6eba2
Neha Navgale <nn78d@mail.umkc.edu>
2019-02-25 11:06:29 -0600

    Added model json file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent}];
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
