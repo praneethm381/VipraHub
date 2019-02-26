6a7d1e40b7d8da8c20eafe6a69bcb23401260f13
pavankotas <pavankotas@gmail.com>
2019-02-25 23:27:14 -0600

    UML DIAGRAM

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
 import {RegistrationComponent} from './registration/registration.component';
+import {UserdashboardComponent} from './userdashboard/userdashboard.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent}];
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'registration', component: RegistrationComponent},
+    {path: 'user', component: UserdashboardComponent}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
