c53b5d42479d38ea0fa5e7ef49b8aabb1ed7eb2f
pavankotas <pavankotas@gmail.com>
2019-02-25 22:02:21 -0600

    login and registartion css changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {RegistrationComponent} from './registration/registration.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registration', component: RegistrationComponent}];
