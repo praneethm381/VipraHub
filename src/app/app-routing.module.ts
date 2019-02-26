eb7309128525656dd1e35f5e231f4b8574adcabf
pavankotas <pavankotas@gmail.com>
2019-02-25 22:34:05 -0600

    css and nav bar fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {RegistrationComponent} from './registration/registration.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'registration', component: RegistrationComponent}];
