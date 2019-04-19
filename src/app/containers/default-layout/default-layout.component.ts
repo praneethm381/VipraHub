fc0347b3c0b3c48a8998163bc998db214c090ede
Neha Navgale <nn78d@mail.umkc.edu>
2019-04-19 11:34:10 -0500

    added search on new header

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { navItems } from '../../_nav';
+import {ViprahubService} from '../../viprahub.service';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 
 @Component({
@@ -12,7 +15,8 @@ export class DefaultLayoutComponent implements OnDestroy {
   public sidebarMinimized = true;
   private changes: MutationObserver;
   public element: HTMLElement;
-  constructor(@Inject(DOCUMENT) _document?: any) {
+  search = { text: ''};
+  constructor(public router: Router, private http: HttpClient, public vipraService: ViprahubService, @Inject(DOCUMENT) _document?: any) {
 
     this.changes = new MutationObserver((mutations) => {
       this.sidebarMinimized = _document.body.classList.contains('sidebar-minimized');
@@ -22,6 +26,11 @@ export class DefaultLayoutComponent implements OnDestroy {
       attributes: true,
       attributeFilter: ['class']
     });
+  }
+  getResults() {
+    this.vipraService.searchText = this.search.text;
+    this.vipraService.getSearchResults(this.search.text);
+    // this.router.navigate(['/search']);
   }
 
   ngOnDestroy(): void {
